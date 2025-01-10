e515cd0e2514d69ad44469b7d96db12f74587934
Otit-code <tmt111021@gmail.com>
2025-01-11 02:17:07 +0700

    3.1 cmt

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// custom inputNumber
 document.addEventListener('DOMContentLoaded', function() {
     const decrementButton = document.querySelector('.decrement');
     const incrementButton = document.querySelector('.increment');
@@ -16,4 +17,39 @@ document.addEventListener('DOMContentLoaded', function() {
             numberInput.value = value + 1;
         }
     });
+});
+
+// show images
+const images = document.querySelectorAll('.images img');
+const prevBtn = document.querySelector('.prev');
+const nextBtn = document.querySelector('.next');
+const closeBtn = document.querySelector('.close');
+const galleryImg = document.querySelector('.gallery-inner img'); // Corrected class name
+const gallery = document.querySelector('.gallery'); 
+let currentImageIndex = 0; // Track the current image index
+
+images.forEach((item, index) => {
+  item.addEventListener('click', () => {
+    currentImageIndex = index;
+    galleryImg.src = item.src; 
+    gallery.classList.add('show');
+  });
+});
+
+closeBtn.addEventListener('click', () => {
+  gallery.classList.remove('show');
+});
+
+prevBtn.addEventListener('click', () => {
+    currentImageIndex = (currentImageIndex - 1 + images.length) % images.length; 
+    galleryImg.src = images[currentImageIndex].src;
+});
+  
+nextBtn.addEventListener('click', () => {
+    currentImageIndex = (currentImageIndex + 1) % images.length; 
+    galleryImg.src = images[currentImageIndex].src;
+});
+
+gallery.addEventListener('wheel', (event) => {
+    event.preventDefault();
 });
